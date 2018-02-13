9b8b0ade7404d076c92d0abdb5b51e3991897a24
Georgiy Mihaylov <georgiy.mihaylov@binomo.com>
2018-02-13 17:49:10 +0300

    add some logic

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Observable, BehaviorSubject } from 'rxjs';
 import { animationFrame } from 'rxjs/scheduler/animationFrame';
-import { DIRECTIONS } from './constants';
+import { DIRECTIONS, POINTS_PER_APPLE, SPEED } from './constants';
 import { Key } from './types';
 
 import {
     createCanvasElement
 } from './canvas';
-import { nextDirection } from './utils';
+import { nextDirection, move, generateSnake } from './utils';
 
 let canvas = createCanvasElement();
 let ctx = canvas.getContext('2d');
@@ -15,6 +15,8 @@ document.body.appendChild(canvas);
 const INITIAL_DIRECTION = DIRECTIONS[Key.RIGHT];
 
 let keydown$ = Observable.fromEvent(document, 'keydown');
+let click$ = Observable.fromEvent(document, 'click');
+let ticks$ = Observable.interval(SPEED);
 
 let direction$ = keydown$
     .map((event: KeyboardEvent) => DIRECTIONS[event.keyCode])
@@ -22,3 +24,19 @@ let direction$ = keydown$
     .scan(nextDirection)
     .startWith(INITIAL_DIRECTION)
     .distinctUntilChanged();
+
+let snakeLength$ = length$
+    .map((event: KeyboardEvent) => DIRECTIONS[event.keyCode])
+    .filter(direction => !!direction)
+    .scan(nextDirection)
+    .startWidth(INITIAL_DIRECTION)
+    .distinctUntilChanged();
+
+let score$ = snakeLength$
+    .startWidth(0)
+    .scan((score, _) => score + POINTS_PER_APPLE);
+
+let snake$: Observable<Arrayy<Point2D>> = ticks$
+    .withLatestFrom(direction$, snakeLength$, (_, direction, snakeLength) => [direction, snakeLength])
+    .scan(move, generateSnake())
+    .share();
--- src/utils.ts
+++ src/utils.ts
@@ -12,3 +12,25 @@ export const nextDirection = (prev, next) => {
 
     return next;
 };
+
+export function move(snake, [direction, snakeLength]) {
+    let nx = snake[0].x;
+    let ny = snake[0].y;
+
+    nx += 1 * direction.x;
+    ny += 1 * direction.y;
+
+    let tail;
+
+    if (snakeLength > snake.length) {
+        tail = {x: nx, y: ny};
+    } else {
+        tail = snake.pop();
+        tail.x = nx;
+        tail.y = ny;
+    }
+
+    snake.unshift(tail);
+
+    return snake;
+}
